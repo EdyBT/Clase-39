1483707a822110ba5abffc19fde4a9aa807f56f9
EdyBT <93800706+EdyBT@users.noreply.github.com>
2022-02-11 17:47:05 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,8 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var track, car1_img, carq_img, car3_img, car4_img;
+var ground;
 
-
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/grund.png")
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
